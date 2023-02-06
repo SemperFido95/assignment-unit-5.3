7bcec03ade6a6064dd34bd0ec82c08e8e00917cc
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-02-06 18:15:54 -0500

    created and tested showCollection()

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,15 +2,19 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 let addToCollection = (title, artist, yearPublished) => {  
+    //creates object with title, artist, and yearPublished properties
     let newSong = {
         title: title,
         artist: artist,
         yearPublished: yearPublished
     };
+    //pushes object into collection array
     collection.push(newSong);
+    //returns newly created object
     return newSong;
 }
 
+//adding ablums to collection array by testing addToCollection()
 console.log(addToCollection('Wasting Light', 'Foo Fighters', 2011));
 console.log(addToCollection('Coastin\'', 'Iration', 2020));
 console.log(addToCollection('Master of Puppets', 'Metallica', 1986));
@@ -19,3 +23,14 @@ console.log(addToCollection('Back In Black', 'AC/DC', 1980));
 console.log(addToCollection('Hybrid Theory', 'Linkin Park', 2000));
 console.log(collection);
 
+let showCollection = array => {
+    //logs number of albums in collection
+    console.log('Number of albums:', array.length);
+    //iterates over each object and logs its properties in the string below
+    array.forEach(album => {
+        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`)
+    });
+}
+
+//testing showCollection()
+showCollection(collection);
